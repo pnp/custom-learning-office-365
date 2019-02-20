a207aa4e177ea368a7ca849a2efafb7fa1d31342
Santy Radhakrishnan (VIDYATECH SOLUTIONS PVT LTD) <v-santr@microsoft.com>
2019-02-19 17:45:09 -0800

    Update web part script

---
+++ webpart/CustomLearningConfiguration.ps1
@@ -0,0 +1,143 @@
+﻿param([PSCredential]$Credentials,
+[string]$TenantName,
+[string]$SiteCollectionName)
+
+
+#region Legal stuff for Telemetry
+
+Write-Host "Microsoft collects active usage data from your organization’s use of Custom Learning for Office 365. Microsoft will use this data to help improve the future Custom Learning for Office 365 solutions. To learn more about Microsoft privacy policies see https://go.microsoft.com/fwlink/?LinkId=521839. If you would like to opt out of this data collection, please type Ctrl-C to stop this script and see Readme file (“Disabling Telemetry Collection” section) for instructions on how to opt out.`n"
+Read-Host "Press Enter to Continue"
+
+$optInTelemetry = $true
+#endregion
+
+# verify the PnP cmdlets we need are installed
+if (!(Get-Command Connect-PnPOnline -ErrorAction SilentlyContinue  ))
+    {
+        Write-Host "Could not find PnP PowerShell cmdlets"
+        Write-Host "Please install them and run this script again"
+        Write-Host "You can install them with the following line:"
+        Write-Host "`nInstall-Module SharePointPnPPowerShellOnline`n"
+        break
+    } 
+
+# Now let's check if $Credentials is empty 
+while ([string]::IsNullOrWhitespace($Credentials)) 
+    {
+        # Prompt the user
+        $Credentials = Get-Credential -Message "Please enter SharePoint Online admin account"
+    }
+
+# Check if tenant name was passed in
+while ([string]::IsNullOrWhitespace($TenantName)) 
+    {
+        # No TenantName was passed, prompt the user
+        $TenantName = Read-Host "Please enter your tenant name: (contoso) "
+        $TestAdminURL = "https://$TenantName-admin.sharepoint.com"
+        # Test that it's a mostly valid URL
+        # This doesn't catch everything
+        if (!([system.uri]::IsWellFormedUriString($TestAdminURL,[System.UriKind]::Absolute)))
+            {
+                Write-Host "$TestAdminURL is not a valid URL."
+                Clear-Variable TenantName
+            }
+    } 
+
+$AdminURL = "https://$TenantName-admin.sharepoint.com"
+
+# Check if $SiteCollectionName was passed in
+if ([string]::IsNullOrWhitespace($SiteCollectionName) )
+    {
+        # No TenantName was passed, prompt the user
+        $SiteCollectionName = Read-Host "Please enter your site collection name: (Press Enter for `'MicrosoftTraining`') "
+        if ([string]::IsNullOrWhitespace($SiteCollectionName)) 
+        {
+            $SiteCollectionName = "MicrosoftTraining"
+        }
+    }
+$clSite = "https://$TenantName.sharepoint.com/sites/$SiteCollectionName"
+
+#region Connect to Admin site.
+try {
+	Connect-PnPOnline -Url $AdminURL -Credentials $Credentials
+} catch {
+	Write-Host "Failed to authenticate to $AdminURL"
+	Write-Host $_
+    break
+}
+    # Need an App Catalog site collection defined for Set-PnPStorageEntity to work
+    if (!(Get-PnPTenantAppCatalogUrl))
+    {
+        Write-Host "Tenant $TenantName must have an App Catalog site defined"
+        Write-Host "Please visit https://social.technet.microsoft.com/wiki/contents/articles/36933.create-app-catalog-in-sharepoint-online.aspx to learn how, then run this setup script again"
+        Write-Host "`n"
+        break
+
+    }
+    
+    # try/catch	- Regular tenant users can auth to admin but can't write values
+    Set-PnPStorageEntity -Key MicrosoftCustomLearningCdn -Value "https://sharepoint.github.io/sp-custom-learning/v2/" -Description "CDN source for Microsoft Content"
+    Get-PnPStorageEntity -Key MicrosoftCustomLearningCdn
+    Set-PnPStorageEntity -Key MicrosoftCustomLearningSite -Value $clSite -Description "Custom Learning Site Collection"
+    Get-PnPStorageEntity -Key MicrosoftCustomLearningSite
+    Set-PnPStorageEntity -Key MicrosoftCustomLearningTelemetryOn -Value $optInTelemetry -Description "Custom Learning Telemetry Collection"
+    Get-PnPStorageEntity -Key MicrosoftCustomLearningTelemetryOn
+
+#endregion
+
+#region Content stuff
+
+try {
+	Connect-PnPOnline -Url $clSite -Credentials $Credentials -ErrorAction Stop
+} catch {
+	if ($_.exception.message -like "*(404) Not Found*") 
+        {
+            Write-Host "Failed to find to $clSite"
+            $CreateIt = Read-Host "Would you like to create it? (y/n)"
+        if ($CreateIt -ne "y")
+            {
+                Write-Host "Please create a Modern Communications site at $clsite and rerun this setup script"
+                break
+            } else {
+                New-PnPSite -Type CommunicationSite -Title "Microsoft Learning" -Url $clSite
+                Write-Host "Please wait for $clsite to be created, then rerun this setup script"
+                break
+                }
+        } else {
+               Write-Host $_.exception.message
+               break
+               }
+        } # end catch
+
+# Get the app
+$id = (Get-PnPApp | Where-Object -Property title -Like -Value "Custom Learning for Office 365").id
+
+# Install it to the site collection
+Install-PnPApp -Identity $id
+
+
+    $clv = Get-PnPListItem -List "Site Pages" -Query "<View><Query><Where><Eq><FieldRef Name='FileLeafRef'/><Value Type='Text'>CustomLearningViewer.aspx</Value></Eq></Where></Query></View>"
+    if($clv -eq $null){
+        $clvPage = Add-PnPClientSidePage "CustomLearningViewer"
+        $clvSection = Add-PnPClientSidePageSection -Page $clvPage -SectionTemplate OneColumn -Order 1
+        Add-PnPClientSideWebPart -Page $clvPage -Component "Custom Learning for Office 365"
+        Set-PnPClientSidePage -Identity $clvPage -Publish
+        $clv = Get-PnPListItem -List "Site Pages" -Query "<View><Query><Where><Eq><FieldRef Name='FileLeafRef'/><Value Type='Text'>CustomLearningViewer.aspx</Value></Eq></Where></Query></View>"
+    }
+    $clv["PageLayoutType"] = "SingleWebPartAppPage"
+    $clv.Update()
+    Invoke-PnPQuery
+    
+    $cla = Get-PnPListItem -List "Site Pages" -Query "<View><Query><Where><Eq><FieldRef Name='FileLeafRef'/><Value Type='Text'>CustomLearningAdmin.aspx</Value></Eq></Where></Query></View>"
+    if($cla -eq $null){
+        $claPage = Add-PnPClientSidePage "CustomLearningAdmin" -Publish
+        $claSection = Add-PnPClientSidePageSection -Page $claPage -SectionTemplate OneColumn -Order 1
+        Add-PnPClientSideWebPart -Page $claPage -Component "Custom Learning Admin for Office 365 Web Part"
+        Set-PnPClientSidePage -Identity $claPage -Publish
+        $cla = Get-PnPListItem -List "Site Pages" -Query "<View><Query><Where><Eq><FieldRef Name='FileLeafRef'/><Value Type='Text'>CustomLearningAdmin.aspx</Value></Eq></Where></Query></View>"
+    }
+    $cla["PageLayoutType"] = "SingleWebPartAppPage"
+    $cla.Update()
+    Invoke-PnPQuery
+#endregion
+
---
+++ webpart/ManualCustomLearningInstall.md
@@ -0,0 +1,43 @@
+# Manually installing and configuring Custom Learning for Office 365
+
+The Microsoft Custom Learning Web Part is build using the [SharePoint Framework](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/sharepoint-framework-overview) version 1.7.1.
+
+To manually install and configure the web part and site collection you will need to complete the following steps:
+
+1. Validate that you have met all the prerequisites.
+1. Install the customlearning.sppkg file in your Office 365 Tenant App Catalog.
+1. Provision/Identify a modern communication site to act as your Custom Learning for Office 365 home site.
+1. Execute a PowerShell script that will configure your tenant with the appropriate artifacts that Custom Learning depends on.
+1. Navigate to the CustomLearningAdmin.aspx site page to load the admin web part to initialize the custom content configuration.
+
+## Prerequisites
+
+You must have set up and configured the tenant-wide App Catalog. Please see [Set up your Office 365 tenant](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/set-up-your-developer-tenant#create-app-catalog-site) and follow the Create app catalog site section. If your tenant-wide App Catalog has already been provisioned you will need access to an account that has rights to upload a package to it to complete this setup process. Generally this is an account with the SharePoint administrator role. If an account with that role does not work, go to the SharePoint admin center and find the Site Collection Administrators for the app catalog site collection and either log in as one of the Site Collection Administrators, or add the SharePoint administrator account that failed to the Site Collection Administrators. You will also need access to an account that is a SharePoint Tenant Admin.
+
+## Install WebPart in Tenant App Catalog
+
+To install the Custom Learning for Office 365 solution you will upload thecustomlearning.sppkg file to the tenant-wide App Catalog and deploy it. Please see [Use the App Catalog to make custom business apps available for your SharePoint Online environment](https://docs.microsoft.com/en-us/sharepoint/use-app-catalog) for detailed instructions on how to add an app to the app catalog.
+
+## Provision/Identify Modern Communication Site
+
+Either identify and existing or provision a new modern communication site in your SharePoint Online tenant. For more information about how to provision a communication site see [Create a communication site in SharePoint Online](https://support.office.com/en-us/article/create-a-communication-site-in-sharepoint-online-7fb44b20-a72f-4d2c-9173-fc8f59ba50eb) and follow the Steps to create a communication site.
+
+You will want to add everyone who should be able to view content to the Visitors group and everyone who should be able to administer custom playlists to the Members group. To configure the site for Custom Learning the first time the user must be either a site collection administrator or part of the Owners group.
+
+Add Custom Learning for Office 365 App to the site collection.
+
+## Execute PowerShell Configuration Script
+
+A PowerShell script `CustomLearningConfiguration.ps1` is included that you will need to execute to create three [tenant properties](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/tenant-properties) that the solution uses. In addition the script creates two [single part app pages](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/web-parts/single-part-app-pages) in the site pages library to host the admin and user web parts at a known location.
+
+### Disabling Telemetry Collection
+
+Part of this solution includes anonymized telemetry tracking opt in, which by default is set to on. If you are performing a manual install and you would like to turn telemetry tracking off, please change the `CustomlearningConfiguration.ps1` script to set the $optInTelemetry variable to $false.
+
+If you are not performing a manual install and would like to turn telemetry tracking off, a seperate script `TelemetryOptOut.ps1` has been included that when run will disable telemetry tracking.
+
+## Initialize web part custom configuration
+
+After the PowerShell script is successfully run, please navigate to `<YOUR-SITE-COLLECTION-URL>/SitePages/CustomLearningAdmin.aspx`. Doing so will initialize the CustomConfig list item that sets up custom learning for its first use.
+
+The configuration is now complete and you can move forward with using Custom Learning for Office 365. Please see the user documentation for more information.
