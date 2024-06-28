351c51365ede5829a0460fcb704fdea6ff313621
Stefan Bauer <stefan.bauer@n8d.at>
2024-06-28 12:35:33 +0200

    chore(docs): Update documentation for v5 beta run

---
+++ installation/ConfigureAnalyticsWebHook.ps1
@@ -0,0 +1,20 @@
+$clSite = "https://sympdcp.sharepoint.com/sites/M365LP"
+Write-Host "Learning Pathways Site: ${clSite}"
+# Url: the URL of the https endpoint that will be called with a post request including payload
+# AnonymizeUser: Default is true, set to false to send unhashed usernames in payload
+# KeyHeader: (Optional) An optional custom header that will be sent with the post request, default is "M365LP-API-KEY"
+# Key: (Optional) An optional custom header value to be sent with the post request, default is null
+$webhookConfig = '{"Url": "https://prod-205.westeurope.logic.azure.com:443/workflows/1af650d8ded24b7593e7522613614c77/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=nkIqFcmFmUVdcYOeRHraES_W7ZIP2ixlstvS7a9H_IU", "AnonymizeUser": true, "KeyHeader": "", "Key": ""}'
+
+#Connect to SharePoint Online Tenant Admin portal
+Connect-PnPOnline -Url $clSite -Interactive
+
+#Get Tenant App Catalog
+$appCatalogUrl = Get-PnPTenantAppCatalogUrl
+
+if ($appCatalogUrl) {  
+
+  Set-PnPStorageEntity -Key MicrosoftCustomLearningWebhookConfig -Value $webhookConfig -Description "Microsoft 365 learning pathways webhook configuration"
+
+  Get-PnPStorageEntity -Key MicrosoftCustomLearningWebhookConfig
+}
